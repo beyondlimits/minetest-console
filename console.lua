335cf83c5c82ff9c644c68ef99d8afaaaba49fc6
TheWild <beyondlimits@10g.pl>
2018-07-14 21:08:07 +0200

    Revert "Delay messages slightly so that results appear after expressions"
    This reverts commit a57fcd5b4e9d54350d4822beffa1fc64f361cffc.

--- console.lua
+++ console.lua
@@ -1,6 +1,5 @@
 local path = minetest.get_modpath(minetest.get_current_modname())
 
-local after            = minetest.after
 local chat_send_player = minetest.chat_send_player
 local colorize         = minetest.colorize
 local concat           = table.concat
@@ -27,12 +26,6 @@ local function pack(...)
 	local result = {...}
 	result.n = select('#', ...)
 	return result
-end
-
-local function chat_send_player_delayed(name, s)
-	after(0, function()
-		chat_send_player(name, s)
-	end)
 end
 
 local g_environ = {
@@ -106,12 +99,12 @@ minetest.register_on_joinplayer(function(player)
 
 	-- Clears the chat window.
 	environ.clear = function()
-		chat_send_player_delayed(name, bunch_of_lfs)
+		chat_send_player(name, bunch_of_lfs)
 	end
 
 	-- Sends message to player who called it.
 	environ.echo = function(s)
-		chat_send_player_delayed(name, s)
+		chat_send_player(name, s)
 	end
 
 	local function load(name)
@@ -144,7 +137,7 @@ minetest.register_on_chat_message(function(name, s)
 	end
 
 	if f == nil then
-		chat_send_player_delayed(name, colorize('#F93', err))
+		chat_send_player(name, colorize('#F93', err))
 		return
 	end
 
@@ -162,9 +155,9 @@ minetest.register_on_chat_message(function(name, s)
 
 	if f[1] then
 		env._ = f[2]
-		chat_send_player_delayed(name, result)
+		chat_send_player(name, result)
 	else
 		env._e = f[2]
-		chat_send_player_delayed(name, '(c@#F93)ERROR: ' .. result)
+		chat_send_player(name, '(c@#F93)ERROR: ' .. result)
 	end
 end)
